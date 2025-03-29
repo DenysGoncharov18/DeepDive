5077ac39b1fa75de77f0f3481c57c27527d4e27d
Denys Goncharov <denys.goncharov.mail@gmail.com>
2025-03-30 01:08:43 +0200

    last commit

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef,OnInit,viewChild,ViewChild } from '@angular/core';
+import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
-
+import{FormsModule} from '@angular/forms'
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ControlComponent],
+  imports: [ControlComponent, ButtonComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements OnInit, AfterViewInit{
 
+
+  @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+    //private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+    ngOnInit(){
+      console.log('OnINIT');
+      console.log(this.form?.nativeElement);
+    }
+
+    ngAfterViewInit(){
+      console.log('AFTER VIeW INIT');
+      console.log(this.form?.nativeElement);
+    }
+
+
+    onSubmit(title:string, ticketText: String){
+    console.log('SUBMITED!!!')
+    console.log(title);
+    console.log(ticketText);
+    this.form?.nativeElement.reset();
+
+
+  // onSubmit(titleElement: HTMLInputElement){
+  //   console.log('SUBMITED!!!')
+  //  // console.dir(titleElement);
+  //   const enteredTitle = titleElement.value;
+  //   console.log('ENTERED TITLE:'+enteredTitle);
+
+  }
 }
+
--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,16 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component,
+          ElementRef,
+          HostBinding,
+          HostListener,
+          input,
+          inject,
+          ViewEncapsulation,
+          ContentChild,
+          contentChild,
+          AfterContentInit,
+          afterRender,
+          afterNextRender
+        } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,10 +20,41 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host:{
-    class:'control'
-  }
+   class:'control',
+   '(click)':'onClick()'
+   }
 
 })
-export class ControlComponent {
+export class ControlComponent implements AfterContentInit{
+
+  //@HostBinding('class') className='control';
+//  @HostListener('click') onClick(){
+//     console.log('Clicked!!!');
+//   }
   label = input.required<string>();
+  private el=inject(ElementRef);
+ // @ContentChild('input') private control?:ElementRef<HTMLInputElement|HTMLTextAreaElement>;
+private control = contentChild<ElementRef<HTMLInputElement|HTMLTextAreaElement>>('input');
+
+constructor(){
+  afterRender(()=>{console.log('afterRender')});
+
+
+  afterNextRender(()=>{console.log('afterNextRender')});
 }
+  ngAfterContentInit() {
+
+  }
+
+ngAfterViewInit(){
+  console.log('AFTER VIeW INIT');
+
+}
+  onClick(){
+    console.log('Clicked!!!');
+    console.log(this.el);
+    console.log(this.control());
+  }
+}
+
+
